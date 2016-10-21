d3bb9015a5c2e446ad03b31a0bca288681f60849
Andrew Fisher <AndrewFishersb@gmail.com>
2016-10-21 10:43:46 -0700

    have a Calorie pipe which will take in an array of meals and a parameter such as under-500 and will loop through all the input and push all information that matches the given parameter into a new array which is returned, if there is not parameter match, the original array is returned

--- app/calories.pipe.ts
+++ app/calories.pipe.ts
@@ -2,7 +2,27 @@ import {Pipe, PipeTransform} from '@angular/core';
 import {Meal} from './meal.model';
 
 @Pipe({
-  pipe: 'calories',
+  name: 'calories',
   pure:false
-
 })
+
+export class CaloriesPipe{
+  transform(input:Meal[],desiredOutput:string){
+    var output: Meal[] = [];
+    for(var i = 0;i<input.length;i++){
+      if(desiredOutput='under-500'){
+        if(input[i].calories<500){
+          output.push(input[i]);
+        }
+      }
+      else if(desiredOutput='over-500'){
+        if(input[i].calories>=500){
+          output.push(input[i]);
+        }
+      }else{
+        return input;
+      }
+    }
+    return output;
+  }
+}
