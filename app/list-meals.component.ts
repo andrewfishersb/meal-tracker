a29a1a63f78e19725ff75acb4a3e7751c5dcfb8e
Andrew Fisher <AndrewFishersb@gmail.com>
2016-10-21 10:50:06 -0700

    set up select to choose how many calories you want to filter by

--- app/list-meals.component.ts
+++ app/list-meals.component.ts
@@ -5,7 +5,13 @@ import {Component, Input, Output, EventEmitter} from "@angular/core";
   selector: 'list-meals',
   template:
   `
-    <div *ngFor="let currentMeal of childListMeals">
+    <select>
+      <option value="all">All</option>
+      <option value="under-500">Under 500 Calories</option>
+      <option value="over-500">Over 500 Calories</option>
+
+    </select>
+    <div *ngFor="let currentMeal of childListMeals|calories:">
       <li>Meal: {{currentMeal.food}}</li>
       <li>Details: {{currentMeal.details}}</li>
       <li>Calories: {{currentMeal.calories}}</li>
